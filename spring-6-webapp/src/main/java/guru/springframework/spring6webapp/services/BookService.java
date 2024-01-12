f82ce2eb6b9ddac3f9b60de9920dcafc31a6aa7d
petregeorgee <petregeorgee@gmail.com>
2024-01-12 14:31:05 +0200

    add controller for author model

--- spring-6-webapp/src/main/java/guru/springframework/spring6webapp/services/BookService.java
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/services/BookService.java
@@ -3,7 +3,6 @@ package guru.springframework.spring6webapp.services;
 import guru.springframework.spring6webapp.domain.Book;
 import org.springframework.stereotype.Service;
 
-//@Service
 public interface BookService
 {
     Iterable<Book> findAll();
