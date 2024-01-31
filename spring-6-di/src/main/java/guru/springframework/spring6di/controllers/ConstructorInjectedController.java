5e2f58958e4f38d4aa756c21633cada3db69010e
petregeorgee <petregeorgee@gmail.com>
2024-01-31 13:39:04 +0200

    finish lesson about spring qualifiers

--- spring-6-di/src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
+++ spring-6-di/src/main/java/guru/springframework/spring6di/controllers/ConstructorInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring6di.controllers;
 
 import guru.springframework.spring6di.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
